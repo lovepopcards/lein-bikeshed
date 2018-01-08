05e27466ba92fa92d8845bbc14a4c8778ba78664
Cody Canning <cocanning11@gmail.com>
2018-01-08 14:46:31 -0500

    try this

--- project.clj
+++ project.clj
@@ -1,13 +1,13 @@
-(defproject lovepopcards/lein-bikeshed "0.4.6"
+(defproject lovepopcards/lein-bikeshed "0.4.6-SNAPSHOT"
   :description (str "A Leiningen plugin designed to tell you your code is bad, "
                     "and that you should feel bad")
   :url "https://github.com/dakrone/lein-bikeshed"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in :leiningen
-  :deploy-repositories {"clojars" {:url           "https://clojars.org/repo"
-                                   :sign-releases false
-                                   :username      :env
-                                   :passphrase    :env}}
+  :deploy-repositories [["clojars" {:url           "https://clojars.org/repo"
+                                    :sign-releases false
+                                    :username      :env
+                                    :passphrase    :env}]]
   :dependencies [[org.clojure/tools.cli "0.3.5"]
                  [org.clojure/tools.namespace "0.2.6"]])
